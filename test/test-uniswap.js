95ebb2704338082cfa93f7fa15f4227891346391
heysourin <sourin07@gmail.com>
2023-01-28 23:47:44 +0530

    updated

---
+++ test/test-uniswap.js
@@ -0,0 +1,9 @@
+const { artifacts } = require("hardhat");
+
+const IERC20 = artifacts.require("IERC20");
+const TestUniswap = artifacts.require("TestUniswap");
+
+contract("TestUniswap", (account) => {
+    //test swap
+    
+});
